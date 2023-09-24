81422d40f13080985ac5e289caa7b443e667676b
TheLore85 <garofalo.digital@gmail.com>
2023-09-24 15:47:03 +0200

    form update

--- src/Components/Gift/Gift.js
+++ src/Components/Gift/Gift.js
@@ -13,14 +13,14 @@ const Gift = () => {
             <div className='divider'></div>
             <img className="logo" src="img/ramona-stefano-dark.png" height='20px'/>
             <div className='divider'></div>
-            <h3>aiutaci a realizzarlo</h3>
+            <h3>Se ti fa piacere, aiutaci a realizzarlo</h3>
           </div>
           <div className={styles.imageMobile}></div>
        
           <div className={styles.ibanContainer}>
             <p>Puoi mandarci il tuo contributo ai seguenti recapiti bancari:</p>
             <div className={styles.iban}>
-              <p><b>IBAN</b> <span>IT-7899NOE0-00000-EO92</span></p>
+              <p><b>IBAN</b> <span>IT30I0100501620000000005724</span></p>
               <img className={styles.copyIcon} src="img/copy-icon.png" height='25px'/>
             </div>
             <p>Nella causale non dimenticarti di inserire il tuo nome cosi possiamo ringraziarti!</p>
